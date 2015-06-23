fa506d882e720029163333164f7c4f1686348189
Dusan Glavaski <dglavask@gmail.com>
2015-06-23 20:25:18 +0200

    Changed the folder structure.

---
+++ src/cpp/main.cpp
@@ -0,0 +1,19 @@
+#include "git_repo.h"
+#include "file_system.h"
+#include <iostream>
+#include <exception>
+
+int main(){
+
+	try{
+		//reate repo if it doesnt exist and initialize it as bare
+		git_repo rep("ja");
+	}catch(std::exception &e){
+		std::cout<< e.what() <<std::endl;
+
+	}
+
+	return 0;
+
+}
+
