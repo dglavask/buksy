cb55dc18c9525eccf8b02b8500f3b8786b75b545
VoidAgent <VoidAgent@users.noreply.github.com>
2015-06-23 14:34:56 +0200

    instructions added

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -6,18 +6,12 @@
 int main(){
 
 	try{
+		//reate repo if it doesnt exist and initialize it as bare
 		git_repo rep("ja");
 	}catch(std::exception &e){
 		std::cout<< e.what() <<std::endl;
 
 	}
-	//file_system fs("ja");
-	//fs.dirExists("ja");
-	//file_system fs2("ja/bla");
-	//file_system fs3("/ja");
-	//std::cout<<fs.getWorkingDir()<<std::endl;
-	//std::cout<<fs2.getWorkingDir()<<std::endl;
-	//std::cout<<fs3.getWorkingDir()<<std::endl;
 
 	return 0;
 
