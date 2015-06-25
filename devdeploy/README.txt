aec2eb5c6120ce97e5b15a72c6c38c2155ad4be8
Dusan Glavaski <dglavask@gmail.com>
2015-06-25 21:35:55 +0200

    Added the virtual server configuration script.

---
+++ devdeploy/README.txt
@@ -0,0 +1,2 @@
+The createvhost.sh script should be run only one time.
+It is used to set up the virtual apache host used for buksy developing.
---
+++ devdeploy/createvhost.sh
@@ -0,0 +1,10 @@
+# This script sets your developement enviroment
+# Preconditions: Apache2, mysql, php5 installed
+# Tested on Ubuntu 15
+sudo mkdir -p /var/www/buksy.dev/public_html
+sudo chown -R $USER:$USER /var/www/buksy.dev/public_html
+sudo echo "# Added by the buksy dev deploy script for setting a apache virtual host" >> /etc/hosts
+sudo echo "127.0.0.1 www.buksy.dev buksy" >> /etc/hosts
+sudo cp buksy.dev.conf /etc/apache2/sites-available/
+sudo a2ensite buksy.dev.conf
+sudo service apache2 reload
